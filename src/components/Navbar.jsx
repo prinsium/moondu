04ec8012965f9bb8034e902f850c975f40f4afa9
3vdi <unitcomics.world@proton.me>
2024-02-25 18:44:17 +0530

    DESKTOP NAV

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,72 @@ export default function Navbar(){
     return(
         <div>
 
-            <ul className="flex flex-row justify-evenly text-gray-300">
-                <li>Products</li>
-                <li>Services</li>
-                <li>Moondu</li>
-                <li>About</li>
-                <li>Contact</li>
+            <ul className="flex flex-row justify-evenly text-gray-300 my-2">
+               
+            <li className="group flex flex-row justify-center items-center gap-2">
+                    <span className="group-hover:bg-red-300 px-2.5 py-1.5 rounded-full group-hover:text-[#131313]">
+                    PRODUCTS
+                    </span>
+                <div className="invisible group-hover:visible">
+
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#131313" class="transition ease-out delay-150 duration-200 group-hover:rotate-45 w-[2.5vw] h-[2.5vw] p-1 bg-red-300 rounded-full">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 10.5 12 3m0 0 7.5 7.5M12 3v18" />
+                    </svg>
+
+                </div>
+                </li>
+
+                <li className="group flex flex-row justify-center items-center gap-2">
+                    <span className="group-hover:bg-red-300 px-2.5 py-1.5 rounded-full group-hover:text-[#131313]">
+                    SERVICES
+                    </span>
+                <div className="invisible group-hover:visible">
+
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#131313" class="transition ease-out delay-150 duration-200 group-hover:rotate-45 w-[2.5vw] h-[2.5vw] p-1 bg-red-300 rounded-full">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 10.5 12 3m0 0 7.5 7.5M12 3v18" />
+                    </svg>
+
+                </div>
+                </li>
+
+                <li className="group flex flex-row justify-center items-center gap-2">
+                    <span className="group-hover:bg-red-300 px-2.5 py-1.5 rounded-full group-hover:text-[#131313]">
+                    MOONDU
+                    </span>
+                <div className="invisible group-hover:visible">
+
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#131313" class="transition ease-out delay-150 duration-200 group-hover:rotate-45 w-[2.5vw] h-[2.5vw] p-1 bg-red-300 rounded-full">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 10.5 12 3m0 0 7.5 7.5M12 3v18" />
+                    </svg>
+
+                </div>
+                </li>
+
+                <li className="group flex flex-row justify-center items-center gap-2">
+                    <span className="group-hover:bg-red-300 px-2.5 py-1.5 rounded-full group-hover:text-[#131313]">
+                    ABOUT US
+                    </span>
+                <div className="invisible group-hover:visible">
+
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#131313" class="transition ease-out delay-150 duration-200 group-hover:rotate-45 w-[2.5vw] h-[2.5vw] p-1 bg-red-300 rounded-full">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 10.5 12 3m0 0 7.5 7.5M12 3v18" />
+                    </svg>
+
+                </div>
+                </li>
+                
+                <li className="group flex flex-row justify-center items-center gap-2">
+                    <span className="group-hover:bg-red-300 px-2.5 py-1.5 rounded-full group-hover:text-[#131313]">
+                    CONTACT
+                    </span>
+                <div className="invisible group-hover:visible">
+
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#131313" class="transition ease-out delay-150 duration-200 group-hover:rotate-45 w-[2.5vw] h-[2.5vw] p-1 bg-red-300 rounded-full">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 10.5 12 3m0 0 7.5 7.5M12 3v18" />
+                    </svg>
+
+                </div>
+                </li>
             </ul>
 
         </div>
